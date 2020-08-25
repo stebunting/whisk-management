3691fc3e1093afab468cb01f5d23d86617b0d52b
Stephen Bunting <stebunting@gmail.com>
2020-08-25 12:08:41 +0200

    Updating Database Delivery Schema from Object to Array

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -3,9 +3,10 @@ const tag = 'whisk-management:adminRoutes';
 
 // Requirements
 const express = require('express');
+const { ObjectID } = require('mongodb');
 const debug = require('debug')(tag);
 const { loginCheck } = require('../controllers/authController')();
-const { setupDb } = require('../../lib/db-control')(tag);
+const { setupDb, getDb } = require('../../lib/db-control')(tag);
 
 function routes() {
   const adminRoutes = express.Router();
@@ -20,6 +21,43 @@ function routes() {
         debug(error);
         return res.send(error.stack);
       }
+    });
+
+  adminRoutes.route('/convertDB')
+    .get(async (req, res) => {
+      const db = getDb();
+      const orders = await db.collection('treatboxOrders')
+        .find({}, { projection: { 'statement.delivery': 1 } })
+        .toArray();
+      orders.forEach(async (order) => {
+        if (Object.keys(order.statement.delivery).length === 0) {
+          order.statement.delivery = [];
+        }
+        if (order.statement.delivery.zone1) {
+          order.statement.delivery.zone1.zone = 1;
+        }
+        if (order.statement.delivery.zone2) {
+          order.statement.delivery.zone2.zone = 2;
+        }
+        if (order.statement.delivery.zone3) {
+          order.statement.delivery.zone3.zone = 3;
+        }
+        if (Object.keys(order.statement.delivery).length > 0) {
+          const newDeliveries = [];
+          Object.keys(order.statement.delivery).forEach((o) => {
+            newDeliveries.push(order.statement.delivery[o]);
+          });
+          order.statement.delivery = newDeliveries;
+        }
+        await db.collection('treatboxOrders').updateOne(
+          { _id: ObjectID(order._id) },
+          { $set: { 'statement.delivery': order.statement.delivery } }
+        );
+      });
+      const newOrders = await db.collection('treatboxOrders')
+        .find({}, { projection: { 'statement.delivery': 1 } })
+        .toArray();
+      return res.json(newOrders);
     });
 
   return adminRoutes;
